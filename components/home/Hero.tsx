4ba0b4f0cf3d61d1d41953882c031a8df6431bf1
Adam Hussain <adam@hussmedia.io>
2024-10-01 13:54:21 -0700

    up to hero component

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
+import HeroCarousel from "./HeroCarousel";
 
 function Hero() {
   return (
-    <div>
-      Hero
-    </div>
-  )
+    <section className="grid grid-cols-1 lg:grid-cols-2 gap-24 items-center">
+      <div>
+        <h1 className="max-w-2xl font-bold text-4xl tracking-tight sm:"></h1>
+      </div>
+      <HeroCarousel />
+    </section>
+  );
 }
 
-export default Hero
+export default Hero;
