e3d3c164dd66ef96b276fe715dbc5a687eea5d50
Adam Hussain <adam@hussmedia.io>
2024-10-04 21:31:14 -0700

    got thru rating input

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -5,6 +5,10 @@ import { formatCurrency } from "@/utils/format";
 import FavoriteToggleButton from "@/components/products/FavoriteToggleButton";
 import AddToCart from "@/components/single-product/AddToCart";
 import ProductRating from "@/components/single-product/ProductRating";
+import SubmitReview from "@/components/reviews/SubmitReview";
+import ProductReviews from "@/components/reviews/ProductReviews";
+
+
 async function SingleProductPage({ params }: { params: { id: string } }) {
   const product = await fetchSingleProduct(params.id);
   const { name, image, company, description, price } = product;
@@ -39,6 +43,8 @@ async function SingleProductPage({ params }: { params: { id: string } }) {
           <AddToCart productId={params.id} />
         </div>
       </div>
+      <ProductReviews productId={params.id}/>
+      <SubmitReview productId={params.id}/>
     </section>
   );
 }
