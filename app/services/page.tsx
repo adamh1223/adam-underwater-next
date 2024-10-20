15684d0811feccb7cdec16e07074fe04cc7bc435
Adam Hussain <adam@hussmedia.io>
2024-10-20 14:48:59 -0700

    styled anchor links

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,15 +1,38 @@
-import React from "react";
+import Sectiontitle from "@/components/global/Sectiontitle";
+import "./services.css";
+import { Button } from "@/components/ui/button";
 
 function page() {
   return (
     <>
-      <div className="flex justify-center pb-5 ps-3">
+      <div className="flex justify-center pb-5 ps-1">
         <img
           src={"/images/services2.png"}
           style={{ height: "110px" }}
           className="pt-3"
         />
       </div>
+
+      <div
+        className="anchors"
+        style={{
+          gridTemplateColumns: "1fr 1fr 1fr",
+        }}
+      >
+        <Button variant="outline" className="anchor">
+          <a href="#video">Underwater Video</a>
+        </Button>
+        <Button variant="outline" className="anchor">
+          <a href="#photo">Underwater Photo</a>
+        </Button>
+        <Button variant="outline" className="anchor">
+          <a href="#dives">Guided Dives</a>
+        </Button>
+      </div>
+
+      <section id="video">
+        <Sectiontitle text="Underwater 8K Video" />
+      </section>
     </>
   );
 }
