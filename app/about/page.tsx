bc3c5715e2690006acf50cfe6a2bb52f0233d44d
Adam Hussain <adam@hussmedia.io>
2024-10-20 18:34:13 -0700

    added logos to about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -8,6 +8,31 @@ function AboutPage() {
           className="pt-5"
         ></img>
       </div>
+      <div className="flex justify-center">
+        <div>
+          <img
+            src={"/images/padi-logo2.png"}
+            style={{ height: "180px" }}
+            className="p-5"
+          />
+          <p>Padi</p>
+        </div>
+        <div>
+          <img
+            src={"/images/aaus-logo.png"}
+            style={{ height: "180px" }}
+            className="p-5"
+          />
+        </div>
+        <div>
+          <img
+            src={"/images/faa-logo.png"}
+            style={{ height: "180px" }}
+            className="p-5"
+          />
+        </div>
+      </div>
+
       <p className="mt-6 text-lg tracking-wide leading-8 max-w-5xl mx-auto text-muted-foreground">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero hic
         distinctio ducimus temporibus nobis autem laboriosam repellat, magni
