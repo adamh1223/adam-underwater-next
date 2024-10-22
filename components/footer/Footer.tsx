44fa0bb7504aee81f9744dd4b99c6251e841c9b4
Adam Hussain <adam@hussmedia.io>
2024-10-22 14:19:15 -0700

    began footer

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -1,26 +1,53 @@
-import {
-  FaFacebook,
-  FaInstagram,
-  FaLinkedin,
-  FaYoutube,
-  FaYoutubeSquare
-} from "react-icons/fa";
+// Footer.jsx
+import { FaFacebook, FaInstagram, FaLinkedin, FaYoutube } from "react-icons/fa";
+import Link from "next/link";
+import "./Footer.css";
 
 function Footer() {
   return (
-    <>
-      <div className="flex justify-center">
-        <div>
-          <hr className="w-[400px]" />
-        </div>
-        <div>
-          <FaInstagram />
-        </div>
-        <div>
-          <hr className="w-[400px]" />
-        </div>
+    <div className="footer-container">
+      <div className="flex items-center justify-center w-full my-8">
+        <div className="flex-1 h-px bg-muted" />
+        <span className="px-4 text-muted-foreground">
+          <div className="social-icons">
+            <Link href="https://instagram.com" target="_blank">
+              <FaInstagram className="social-icon" />
+            </Link>
+            <Link href="https://facebook.com" target="_blank">
+              <FaFacebook className="social-icon" />
+            </Link>
+            <Link href="https://youtube.com" target="_blank">
+              <FaYoutube className="social-icon" />
+            </Link>
+            <Link href="https://linkedin.com" target="_blank">
+              <FaLinkedin className="social-icon" />
+            </Link>
+          </div>
+        </span>
+        <div className="flex-1 h-px bg-muted" />
       </div>
-    </>
+
+      {/*
+
+      <div className="social-icons">
+        <Link href="https://instagram.com" target="_blank">
+          <FaInstagram className="social-icon" />
+        </Link>
+        <Link href="https://facebook.com" target="_blank">
+          <FaFacebook className="social-icon" />
+        </Link>
+        <Link href="https://youtube.com" target="_blank">
+          <FaYoutube className="social-icon" />
+        </Link>
+        <Link href="https://linkedin.com" target="_blank">
+          <FaLinkedin className="social-icon" />
+        </Link>
+      </div>
+
+      <div className="line-container">
+        <hr className="horizontal-line" />
+      </div> */}
+    </div>
   );
 }
 
