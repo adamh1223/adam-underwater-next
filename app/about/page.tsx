37ab319256ab22784ce27f64f1bc65b9d7a83fb3
Adam Hussain <adam@hussmedia.io>
2024-10-23 15:13:05 -0700

    worked on about page layout

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -8,27 +8,32 @@ function AboutPage() {
           src={"/images/aboutme3.png"}
           style={{ height: "110px" }}
           className="pt-5"
-        ></img>
+        />
       </div>
-      <div className="headshot-container flex flex-row justify-center">
-        <img src={"/images/headshot3.png"} className="pt-5 headshot"></img>
-        <div className="about-icons flex justify-center mx-3">
-          <div className="about-icon-container pe-5">
-            <img src={"/images/padi-logo2.png"} className="about-icon p-5" />
-            <p>PADI Open Water Scuba Instructor</p>
+
+      <div className="about-container">
+        <img src={"/images/headshot3.png"} className="pt-5 headshot" />
+
+        <div className="about-icon-wrapper">
+          <div className="about-icon-container">
+            <img src={"/images/padi-logo2.png"} className="padi-icon" />
+            <p className="padi-description text-lg">
+              PADI Open Water Scuba Instructor
+            </p>
           </div>
-          <div className="about-icon-container px-5">
-            <img src={"/images/aaus-logo.png"} className="about-icon p-5" />
-            <p>AAUS Scientific Diver</p>
+
+          <div className="about-icon-container">
+            <img src={"/images/aaus-logo.png"} className="about-icon" />
+            <p className="text-lg description">AAUS Scientific Diver</p>
           </div>
           <div className="about-icon-container">
-            <img src={"/images/faa-logo.png"} className="about-icon p-5" />
-            <p>FAA Part 107 Certified Drone Pilot</p>
+            <img src={"/images/faa-logo.png"} className="about-icon" />
+            <p className="text-lg description">FAA Part 107 Drone Operator</p>
           </div>
         </div>
       </div>
 
-      <p className="mt-6 text-lg tracking-wide leading-8 max-w-5xl mx-auto text-muted-foreground">
+      <p className="mt-6 text-lg tracking-wide leading-8 max-w-5xl mx-auto ">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero hic
         distinctio ducimus temporibus nobis autem laboriosam repellat, magni
         fugiat minima excepturi neque, tenetur possimus nihil atque! Culpa nulla
@@ -37,4 +42,5 @@ function AboutPage() {
     </section>
   );
 }
+
 export default AboutPage;
