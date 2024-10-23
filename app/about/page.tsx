bc51697da0b50b3e10221f21a7270de1035e88ae
Adam Hussain <adam@hussmedia.io>
2024-10-23 12:26:49 -0700

    started stylizing about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,35 +1,30 @@
+import "./about.css";
+
 function AboutPage() {
   return (
     <section>
-      <div className="flex justify-center ms-[-10px]">
+      <div className="flex justify-center">
         <img
           src={"/images/aboutme3.png"}
           style={{ height: "110px" }}
           className="pt-5"
         ></img>
       </div>
-      <div className="flex justify-center">
-        <div>
-          <img
-            src={"/images/padi-logo2.png"}
-            style={{ height: "180px" }}
-            className="p-5"
-          />
-          <p>Padi</p>
-        </div>
-        <div>
-          <img
-            src={"/images/aaus-logo.png"}
-            style={{ height: "180px" }}
-            className="p-5"
-          />
-        </div>
-        <div>
-          <img
-            src={"/images/faa-logo.png"}
-            style={{ height: "180px" }}
-            className="p-5"
-          />
+      <div className="headshot-container flex flex-row justify-center">
+        <img src={"/images/headshot3.png"} className="pt-5 headshot"></img>
+        <div className="about-icons flex justify-center mx-3">
+          <div className="about-icon-container pe-5">
+            <img src={"/images/padi-logo2.png"} className="about-icon p-5" />
+            <p>PADI Open Water Scuba Instructor</p>
+          </div>
+          <div className="about-icon-container px-5">
+            <img src={"/images/aaus-logo.png"} className="about-icon p-5" />
+            <p>AAUS Scientific Diver</p>
+          </div>
+          <div className="about-icon-container">
+            <img src={"/images/faa-logo.png"} className="about-icon p-5" />
+            <p>FAA Part 107 Certified Drone Pilot</p>
+          </div>
         </div>
       </div>
 
