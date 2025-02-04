6bd71133951cd0ce4105af8b215a1556d3deada5
Adam Hussain <adam@hussmedia.io>
2025-02-04 14:29:23 -0800

    kjlhjlk

--- components/eproducts/EProductPreview.tsx
+++ components/eproducts/EProductPreview.tsx
@@ -1,8 +1,7 @@
 "use client";
 
 import { EProduct } from "@prisma/client";
-import Image from "next/image";
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 
 function EProductPreview({
   EProduct,
@@ -12,55 +11,58 @@ function EProductPreview({
   extraClassName?: string;
 }) {
   const [isHovered, setIsHovered] = useState(false);
+  const [thumbnailSize, setThumbnailSize] = useState({ width: 0, height: 0 });
+  const imgRef = useRef<HTMLImageElement>(null);
+
+  useEffect(() => {
+    if (imgRef.current) {
+      setThumbnailSize({
+        width: imgRef.current.clientWidth,
+        height: imgRef.current.clientHeight,
+      });
+    }
+  }, [isHovered]);
+
   const imageClassName = extraClassName
     ? `${extraClassName} `
-    : "flex items-center justify-center rounded w-full object-cover transform group-hover:scale-105 transition-transform duration-500";
+    : "flex items-center justify-center rounded w-full object-cover transform group-hover:scale-105 transition-transform duration-500 aspect-video";
+
   const { thumbnail, WMVideoLink } = EProduct;
+
   return (
     <div
-      className="h-full w-full"
-      onMouseEnter={() => {
-        setIsHovered(true);
-      }}
-      onMouseLeave={() => {
-        setIsHovered(false);
-      }}
+      className="h-full w-full rounded overflow-hidden"
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
     >
       {isHovered ? (
-        <iframe
-          src={`https://player.vimeo.com/video/${WMVideoLink}?autoplay=1&muted=1&background=1&badge=0&amp;autopause=0`}
-          allow="autoplay; loop;"
-          onMouseLeave={() => {
-            setIsHovered(false);
-          }}
-          className="rounded"
-        ></iframe>
+        <div className="w-full h-full rounded overflow-hidden">
+          <iframe
+            src={`https://player.vimeo.com/video/${WMVideoLink}?autoplay=1&muted=1&background=1&badge=0&autopause=0`}
+            allow="autoplay; loop;"
+            style={{
+              width: thumbnailSize.width,
+              height: thumbnailSize.height,
+              borderRadius: "12px",
+            }}
+            className="w-full h-full object-cover rounded"
+          ></iframe>
+        </div>
       ) : (
         <>
-          {thumbnail != null && thumbnail.length > 0 && (
+          {thumbnail && thumbnail.length > 0 && (
             <img
+              ref={imgRef}
               src={thumbnail}
-              alt={"name"}
+              alt="name"
               sizes="(max-width:768px) 100vw, (max-width:1200px) 50vw, 33vw"
-              //   className="w-full rounded object-cover"
               className={imageClassName}
             />
-          )}{" "}
+          )}
         </>
       )}
     </div>
   );
-}
-
-{
-  /* <iframe
-  src="https://player.vimeo.com/video/1018553050?autoplay=1&loop=1&muted=1&background=1"
-  frameBorder="0"
-  allow="autoplay; fullscreen; picture-in-picture"
-  allowFullScreen
-  className={`video ${isVideoReady ? "visible" : ""}`}
-  title="Background Video"
-></iframe>; */
 }
 
 export default EProductPreview;
